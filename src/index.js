0c2fcabf591cdcd32f80f032f65980f90a21ad08
Winson <807965894@qq.com>
2020-02-13 16:06:55 +0800

    6666

--- src/index.js
+++ src/index.js
@@ -20,13 +20,19 @@ class Board extends React.Component {
         super(props);
         this.state = {
             squares: Array(9).fill(null),
+            xIsNext: true,
         };
     }
 
     handleClick(i) {
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
         this.setState({squares: squares});
+        this.state.xIsNext = !this.state.xIsNext;
+        console.log(squares[i])
     }
 
     renderSquare(i) {
@@ -39,7 +45,17 @@ class Board extends React.Component {
     }
 
     render() {
-        const status = 'Next player: X';
+        let status;
+        const winner = calculateWinner(this.state.squares)
+        if (winner){
+            status = 'winner is :' + winner;
+
+        }
+        else {
+            status = 'Next player:' + (this.state.xIsNext?'X':'O');
+
+        }
+
 
         return (
             <div>
@@ -63,7 +79,7 @@ class Game extends React.Component {
         return (
             <div className="game">
                 <div className="game-board">
-                    <Board />
+                    <Board/>
                 </div>
                 <div className="game-info">
                     <div>{/* status */}</div>
@@ -75,8 +91,27 @@ class Game extends React.Component {
 }
 
 // ========================================dsdsdfdfsdds
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
 
 ReactDOM.render(
-    <Game />,
+    <Game/>,
     document.getElementById('root')
 );
